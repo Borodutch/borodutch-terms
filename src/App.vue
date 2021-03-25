48717ddd387df8d958e3fd269271fb09ec789e09
backmeupplz <backmeupplz@gmail.com>
2021-03-25 08:34:44 -0700

    update

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+v-app
+  v-main
+    .v-container.pa-4
+      v-layout(column)
+        h2
+          | {{ "Common terms and conditions of use of projects listed at " }}
+          a(href='https://borodutch.com') borodutch.com
+          | .
+        h2 1. Terms
+        p
+          | By accessing this Website, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.
+        h2 2. Disclaimer
+        p
+          | All the materials on Service’s Website are provided "as is". Service makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, Service does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.
+        h2 3. Limitations
+        p
+          | Service or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on Service’s Website, even if Service or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.
+        h2 4. Revisions and Errata
+        p
+          | The materials appearing on Service’s Website may include technical, typographical, or photographic errors. Service will not promise that any of the materials in this Website are accurate, complete, or current. Service may change the materials contained on its Website at any time without notice. Service does not make any commitment to update the materials.
+        h2 5. Links
+        p
+          | Service has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by Service of the site. The use of any linked website is at the user’s own risk.
+        h2 6. Site Terms of Use Modifications
+        p
+          | Service may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.
+        h2 7. Your Privacy
+        p
+          | Please read
+          | {{ " " }}
+          a(href='https://privacy.borodutch.com') our Privacy Policy
+          | .
+        h2 8. Governing Law
+        p
+          | Any claim related to Service's Website shall be governed by the laws of ca without regards to its conflict of law provisions.
+        h2 9. Encryption
+        p
+          | In case if the user enables encryption, there is no way to decrypt the data without the password specified by the user.
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {}
+</script>
